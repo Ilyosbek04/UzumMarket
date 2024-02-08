56c4811027bd094b384ab1ec903e99c6a21b6de4
Ilyosbek04 <habibullayev0329@gmail.com>
2024-02-08 18:10:24 +0500

    lorem ipsum

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -3,12 +3,12 @@ import "./Carousel.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-// import { Pagination, Autoplay } from "swiper/modules";
-// import photo1 from "../../assets/main_page_banner (7).jpg";
-// import photo2 from "../../assets/main_page_banner (8).jpg";
-// import photo3 from "../../assets/main_page_banner (9).jpg";
-// import photo4 from '../../assets/main_page_banner (10).jpg'
-// import photo5 from '../../assets/main_page_banner (11).jpg'
+import { Pagination, Autoplay } from "swiper/modules";
+import photo1 from "../../assets/main_page_banner (7).jpg";
+import photo2 from "../../assets/main_page_banner (8).jpg";
+import photo3 from "../../assets/main_page_banner (9).jpg";
+import photo4 from '../../assets/main_page_banner (10).jpg'
+import photo5 from '../../assets/main_page_banner (11).jpg'
 
 function Carousel() {
   return (
@@ -23,7 +23,9 @@ function Carousel() {
         modules={[Pagination, Autoplay]}
         className="carousel__inner"
       >
-    
+        <SwiperSlide>
+          <img src="https://images.uzum.uz/cm1d5m1uf2i84h4paoc0/main_page_banner.jpg" />
+        </SwiperSlide>
         <SwiperSlide>
           <img
             className="carousel__desktop"
